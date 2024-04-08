943808580af0411a39308147828566101c58920d
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2024-04-08 19:40:44 +0100

    Update SQL.php

--- Database/SQL.php
+++ Database/SQL.php
@@ -19,6 +19,7 @@ class SQL
                 $db_pass,
                 $db_name
             );
+            $this->SQL->set_charset("utf8");
         } catch (Exception $e) {
             echo $e->getMessage();
             exit;
