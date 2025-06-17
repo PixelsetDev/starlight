11b12e38c1daf1c58aed84600685e911b872d87b
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-06-17 10:23:23 +0100

    Add CSV type

--- HTTP/Type.php
+++ HTTP/Type.php
@@ -7,4 +7,8 @@ class Type
     public function json(): void {
         header("Content-type: application/json; charset=utf-8");
     }
+    
+    public function csv(): void {
+        header("Content-type: text/csv; charset=utf-8");
+    }
 }
